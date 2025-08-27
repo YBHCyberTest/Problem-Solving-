976f0e1ee8ed11b39216fa0c2bf7c1e770c575ca
Yaser Bin Humaidah <55802354+YBHCyberTest@users.noreply.github.com>
2025-08-27 09:45:49 +0300

    Update cemara.py

--- Problem Solving/cemara.py
+++ Problem Solving/cemara.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-# اختر الفيديو المناسب (video0 أو video1)
-cap = cv2.VideoCapture(0)  # جرب 0 أو 1 لو الشاشة سوداء
+# اختر الفيديو: video0 أو video1
+cap = cv2.VideoCapture('/dev/video1')
 
 if not cap.isOpened():
     print("❌ لم أتمكن من فتح الكاميرا")
@@ -11,17 +11,14 @@ if not cap.isOpened():
 while True:
     ret, frame = cap.read()
     if not ret:
-        print("❌ لم أتمكن من قراءة الإطار")
+        print("❌ لم أتمكن من قراءة الفريم")
         break
 
-    # تحويل الصورة إلى grayscale
+    # تحويل الصورة إلى درجات حرارة ملونة
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # تطبيق colormap حراري
     thermal = cv2.applyColorMap(gray, cv2.COLORMAP_INFERNO)
 
-    # عرض الصورة
-    cv2.imshow("UTi720M Thermal View", thermal)
+    cv2.imshow('Thermal Camera', thermal)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
